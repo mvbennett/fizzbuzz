8cf8830cab663ec14ee76ab4ab67f3e4c6428a81
Michael Bennett <mvbennett9@gmail.com>
2023-03-06 01:31:10 -0500

    made a fizzbuzz function in python

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+def fizz_buzz(array):
+    for num in array:
+        if num % 3 == 0 and num % 5 == 0:
+            print('FizzBuzz')
+        elif num % 3 == 0:
+            print('Fizz')
+        elif num % 5 == 0:
+            print('Buzz')
+        else:
+            print(num)
+
+print(fizz_buzz(range(1,101)))
